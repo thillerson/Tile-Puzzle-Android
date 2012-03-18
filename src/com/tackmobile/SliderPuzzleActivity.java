be61d8880719267e62b753b9aba6b77a30a3cbb4
Tony Hillerson <tony.hillerson@gmail.com>
2012-03-18 15:30:14 -0600

    Commit of generated project

---
+++ src/com/tackmobile/SliderPuzzleActivity.java
@@ -0,0 +1,13 @@
+package com.tackmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SliderPuzzleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
