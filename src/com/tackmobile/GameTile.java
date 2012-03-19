8f210d7759c3487ecf07c384aa4505850d97e191
Tony Hillerson <tony.hillerson@gmail.com>
2012-03-18 21:20:33 -0600

    Drawing the tiles 4x4 with a horrible red color.

--- src/com/tackmobile/GameTile.java
+++ src/com/tackmobile/GameTile.java
@@ -1,7 +1,19 @@
 package com.tackmobile;
 
+import com.tackmobile.GameboardView.Coordinate;
+
+import android.graphics.Color;
 import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 
 public class GameTile extends ShapeDrawable {
+	
+	public Coordinate coordinate;
+
+	public GameTile(Coordinate coordinate) {
+		super( new RoundRectShape( new float[]{4,4,4,4,4,4,4,4}, null, null ) );
+		getPaint().setColor(Color.RED);
+		this.coordinate = coordinate;
+	}
 
 }
--- src/com/tackmobile/GameboardView.java
+++ src/com/tackmobile/GameboardView.java
@@ -1,26 +1,102 @@
 package com.tackmobile;
 
+import java.util.HashSet;
+
+import roboguice.util.Ln;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GameboardView extends View {
 	
-	protected GameTile singleTile;
+	protected Size tileSize;
+	protected Rect gameboardRect;
+	protected HashSet<GameTile> tiles;
 
 	public GameboardView(Context context, AttributeSet attrSet) {
 		super(context, attrSet);
-		singleTile = new GameTile();
-		singleTile.getPaint().setColor(Color.RED);
-		singleTile.setBounds(100, 100, 200, 200);
 	}
 	
 	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+		determineGameboardSizes();
+	}
+
+	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		singleTile.draw(canvas);
+		drawTiles(canvas);
+	}
+	
+	protected void drawTiles(Canvas canvas) {
+		for (GameTile tile : tiles) {
+			placeTile(tile, canvas);
+		}
+	}
+	
+	protected void placeTile(GameTile tile, Canvas canvas) {
+		tile.setBounds(rectForCoordinate(tile.coordinate));
+		tile.draw(canvas);
+	}
+
+	protected void createTiles() {
+		tiles = new HashSet<GameTile>();
+		for (int rowI=0; rowI<4; rowI++) {
+			for (int colI=0; colI<4; colI++) {
+				createTileAtCoordinate( new Coordinate(rowI, colI) );
+			}
+		}
+	}
+
+	protected void createTileAtCoordinate(Coordinate coordinate) {
+		tiles.add(new GameTile(coordinate));
+	}
+
+	protected void determineGameboardSizes() {
+		int viewWidth = getWidth();
+		int viewHeight = getHeight();
+		Ln.d("width %d, height %d", viewWidth, viewHeight);
+		// ostensibly tiles can be sized based on view geometry. Hardcode for now.
+		tileSize = new Size(68, 68);
+		int gameboardWidth = tileSize.width * 4;
+		int gameboardHeight = tileSize.height * 4;
+		int gameboardTop = viewHeight/2 - gameboardHeight/2;
+		int gameboardLeft = viewWidth/2 - gameboardWidth/2;
+		gameboardRect = new Rect(gameboardLeft, gameboardTop, gameboardLeft + gameboardWidth, gameboardTop + gameboardHeight);
+		createTiles();
+	}
+
+	protected Rect rectForCoordinate(Coordinate coordinate) {
+		int top = (coordinate.row * tileSize.height) + gameboardRect.top;
+		int left = (coordinate.column * tileSize.width) + gameboardRect.left;
+		return new Rect(left, top, left + tileSize.width, top + tileSize.height);
+	}
+	
+	public class Size {
+		
+		public int width;
+		public int height;
+		
+		public Size(int width, int height) {
+			this.width = width;
+			this.height = height;
+		}
+		
+	}
+
+	public class Coordinate {
+		
+		public int row;
+		public int column;
+
+		public Coordinate(int row, int column) {
+			this.row = row;
+			this.column = column;
+		}
+
 	}
 
 }
